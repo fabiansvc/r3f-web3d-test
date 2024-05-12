53dc3be79b016d48c3b7d040e1aec61e0cb87faa
Fabian Stiven Valencia Cordoba <fabian.cordoba@correounivalle.edu.co>
2024-05-11 23:12:23 -0500

    Ball collided with fox implemented

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,15 +1,49 @@
 import { Canvas } from "@react-three/fiber";
-import Model from "./Model";
-import { OrbitControls, Sky } from "@react-three/drei";
+import Fox from "./world/Fox";
+import { KeyboardControls, Sky } from "@react-three/drei";
+import { Physics } from "@react-three/rapier";
+import Floor from "./world/Floor";
+import Ecctrl from "ecctrl";
+import { useMemo } from "react";
+import Player from "./world/Player";
 
 export default function Experience() {
+  const map = useMemo(
+    () => [
+      { name: "forward", keys: ["ArrowUp", "KeyW"] },
+      { name: "backward", keys: ["ArrowDown", "KeyS"] },
+      { name: "leftward", keys: ["ArrowLeft", "KeyA"] },
+      { name: "rightward", keys: ["ArrowRight", "KeyD"] },
+      { name: "jump", keys: ["Space"] },
+      { name: "run", keys: ["Shift"] },
+    ],
+    []
+  );
+
+  const onCollisionBall = ({other}) => {
+    if(other.rigidBodyObject.name === "Fox") {
+      console.log("Ball collided with Fox");
+    }
+  };
+
   return (
-    <Canvas>
-      <OrbitControls />
-      <Sky />
-      <ambientLight />
-      <directionalLight position={[10, 10, 5]} intensity={5} />
-      <Model />
-    </Canvas>
+    <KeyboardControls map={map}>
+      <Canvas>
+        <Sky />
+        <ambientLight />
+        <directionalLight position={[10, 10, 5]} intensity={5} />
+        <Physics debug>
+          <Floor />
+          <Fox position={[0, 0, 3]} />
+          <Ecctrl
+            position={[0, 1, -4]}
+            name="ball"
+            onCollisionEnter={(e) => onCollisionBall(e)}
+          >
+            <Player />
+          </Ecctrl>
+        </Physics>
+      </Canvas>
+    </KeyboardControls>
   );
 }
